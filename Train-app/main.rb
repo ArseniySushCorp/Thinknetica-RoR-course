57b14279558b1796c2c66ba4dc6e787b34c2e3eb
Arseniy Suschevskiy <arseniysuschev@gmail.com>
2021-04-10 23:33:12 +0300

    Route methods fixes

--- Train-app/main.rb
+++ Train-app/main.rb
@@ -8,79 +8,16 @@ require_relative './train'
 freight_train = Train.new('4242', FREIGHT, 0)
 passenger_train = Train.new('4343', PASSENGER, 0)
 
-puts
-puts 'brake'
-freight_train.brake
-freight_train.speed_up
-freight_train.brake
-
-puts
-puts 'add_carriage'
-freight_train.add_carriage
-freight_train.speed_up
-freight_train.add_carriage
-freight_train.brake
-
-puts
-puts 'delete_carriage'
-freight_train.speed_up
-freight_train.delete_carriage
-freight_train.brake
-freight_train.delete_carriage
-
-puts
-puts 'take_route near_stations move'
-freight_train.near_stations
-freight_train.take_route(%w[Nagoya Toyohashi Okazaki Seto Toyokawa Tsushima])
-freight_train.near_stations
-freight_train.move('Toyohashi')
-freight_train.near_stations
-
-puts
 puts 'RailwayStation.new'
 nagoya = RailwayStation.new('Nagoya')
 tsushima = RailwayStation.new('Tsushima')
 okazaki = RailwayStation.new('Okazaki')
+puts okazaki.name
+puts tsushima.name
 seto = RailwayStation.new('Seto')
-
-puts
-puts 'Nagoya take_train freight_train'
-nagoya.take_train(freight_train)
-puts nagoya.trains_list
-puts 'Nagoya take_train passenger_train'
-nagoya.take_train(passenger_train)
-puts nagoya.trains_list
-
-puts
-puts 'Nagoya send_train freight_train'
-nagoya.send_train(freight_train)
-puts nagoya.trains_list
 
 puts
 puts 'Route.new'
 tokyo_route = Route.new(nagoya, tsushima)
-tokyo_route.first
-tokyo_route.last
-tokyo_route.show_all
-
-puts
-puts 'add_station'
 tokyo_route.add(okazaki)
-tokyo_route.show_all
-tokyo_route.add('4242')
-tokyo_route.show_all
-
-puts
-puts 'delete_station'
 tokyo_route.delete(okazaki)
-tokyo_route.show_all
-tokyo_route.delete('4242')
-tokyo_route.show_all
-
-puts
-puts 'Train take route instance_of? Route'
-tokyo_route.add(okazaki)
-tokyo_route.add(seto)
-tokyo_route.show_all
-passenger_train.take_route(tokyo_route)
-puts passenger_train.route
--- Train-app/route.rb
+++ Train-app/route.rb
@@ -2,57 +2,26 @@ class Route
   attr_reader :stations
 
   def initialize(first, last)
-    @stations = instance_of_station?(first) && instance_of_station?(last) ? [first.name, last.name] : [first, last]
-
-    puts "Route created from #{@stations.first} to #{@stations.last}"
+    @stations = [first, last]
   end
 
   def first
-    puts "First station is #{@stations.first}"
+    @stations.first
   end
 
   def last
-    puts "Last station is #{@stations.last}"
+    @stations.last
   end
 
   def intermediate
-    puts "Intermediate stations is #{@stations[1..-2]}"
+    @stations[1..-2]
   end
 
-  def add(value)
-    station = define_station(value)
-    insert(station)
-
-    puts "New station: #{station} added to route"
-  end
-
-  def delete(value)
-    station = define_station(value)
-
-    if @stations.include?(station)
-      @stations.delete(station)
-
-      puts "Station #{station} deleted"
-    else
-      puts "No such station: #{station} in the route"
-    end
-  end
-
-  def show_all
-    @stations.each { |station| puts station }
-  end
-
-  private
-
-  def instance_of_station?(value)
-    value.instance_of? RailwayStation
-  end
-
-  def insert(station)
+  def add(station)
     @stations.insert(-2, station)
   end
 
-  def define_station(value)
-    instance_of_station?(value) ? value.name : value
+  def delete(station)
+    @stations.delete(station)
   end
 end
