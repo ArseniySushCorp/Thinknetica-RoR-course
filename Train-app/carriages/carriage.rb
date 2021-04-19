28470f81207f5cfea08f9080f80dc5e6e018a05d
Arseniy Suschevskiy <arseniysuschev@gmail.com>
2021-04-19 11:38:03 +0300

    Validate carriages, changes in create_menu

--- Train-app/carriages/carriage.rb
+++ Train-app/carriages/carriage.rb
@@ -5,5 +5,20 @@ class Carriage
   def initialize(number, type)
     @type = type
     @number = number
+    validate!
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  private
+
+  def validate!
+    raise 'Max value for carriage number is 10 symbols' if number.to_s.length > 10
+
+    true
   end
 end
