46109b244a112de6b4d3c96803e8fab55704580e
Arseniy Suschevskiy <arseniysuschev@gmail.com>
2021-05-02 18:48:35 +0300

    Add count for carriages

--- Train-app/carriages/cargo_carriage.rb
+++ Train-app/carriages/cargo_carriage.rb
@@ -1,6 +1,18 @@
 class CargoCarriage < Carriage
-  def initialize(number, type = :cargo)
+  attr_reader :volume
+
+  def initialize(number, max_volume, type = :cargo)
     @number = number
+    @max_volume = max_volume
+    @volume = 0
     super
+  end
+
+  def fill_volume(value)
+    @volume += value
+  end
+
+  def free_volume
+    @max_volume - @volume
   end
 end
--- Train-app/carriages/passenger_carriage.rb
+++ Train-app/carriages/passenger_carriage.rb
@@ -1,17 +1,17 @@
 class PassengerCarriage < Carriage
-  attr_reader :free_seats
+  attr_reader :seats
 
   def initialize(number, max_seats, type = :passenger)
     super
     @max_seats = max_seats
-    @free_seats = @max_seats
+    @seats = 0
   end
 
   def take_seat
-    @free_seats -= 1
+    @seats += 1
   end
 
-  def seats
-    @max_seats - @free_seats
+  def free_seats
+    @max_seats - @seats
   end
 end
