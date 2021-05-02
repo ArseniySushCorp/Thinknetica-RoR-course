e7aaae1547655142b0b2670f6c3977d94dc32de9
Arseniy Suschevskiy <arseniysuschev@gmail.com>
2021-05-02 18:25:50 +0300

    Add seats and take seats to carriage

--- Train-app/carriages/passenger_carriage.rb
+++ Train-app/carriages/passenger_carriage.rb
@@ -1,6 +1,17 @@
 class PassengerCarriage < Carriage
-  def initialize(number, type = :passenger)
-    @number = number
+  attr_reader :free_seats
+
+  def initialize(number, max_seats, type = :passenger)
     super
+    @max_seats = max_seats
+    @free_seats = @max_seats
+  end
+
+  def take_seat
+    @free_seats -= 1
+  end
+
+  def seats
+    @max_seats - @free_seats
   end
 end
